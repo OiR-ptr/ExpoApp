d7fb7dc79f6220cc9541e56757d79afb433da425
OiR-ptr <myr.ryusuke@gmail.com>
2020-07-09 14:50:09 +0900

    :star: initialized game rule

--- src/GameScreenTop.jsx
+++ src/GameScreenTop.jsx
@@ -1,6 +1,10 @@
 import React from "react";
 import { View, Image, StyleSheet } from "react-native";
 import Number0 from "./_images/number_0.png";
+import { useEffect } from "react";
+import { useDispatch } from "react-redux";
+import { useCallback } from "react";
+import { initializeGameruleEvent } from "hit_and_blow/src/actions/GameActions";
 
 const styles = StyleSheet.create({
   container: {
@@ -12,7 +16,12 @@ const styles = StyleSheet.create({
   },
 });
 
-const GameScreen = () => {
+const GameScreen = (props) => {
+  const { initializeGameRule } = props;
+  useEffect(() => {
+    initializeGameRule(4, 9, false);
+  }, [initializeGameRule]);
+
   return (
     <View style={styles.container}>
       <Image style={styles.tinyLogo} source={Number0} />
@@ -20,4 +29,28 @@ const GameScreen = () => {
   );
 };
 
-export default GameScreen;
+const useStateProps = () => {
+  return {};
+};
+
+const useDispatchProps = () => {
+  const dispatch = useDispatch();
+
+  const initializeGameRule = useCallback(
+    (dialLength, maxNumber, hasDuplicable) => {
+      dispatch(initializeGameruleEvent(dialLength, maxNumber, hasDuplicable));
+    },
+    [dispatch]
+  );
+
+  return {
+    initializeGameRule,
+  };
+};
+
+const GameScreenContainer = (props) => {
+  const _props = { ...useStateProps(), ...useDispatchProps(), ...props };
+  return <GameScreen {..._props} />;
+};
+
+export default GameScreenContainer;
